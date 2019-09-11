20124e01fa841c17c420afd54c6309150310c104
Sss41 <silvana.souza63@gmail.com>
2019-09-11 17:15:50 -0300

    Commitando ao final do dia

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using System.Diagnostics;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopwatch = new Stopwatch();
+            List<int> lista = new List<int>();
+            int i = 3;
+            stopwatch.Start();
+            while (i <= 1000000)
+            {
+                int j = 3;
+                while (j < i && i % j != 0)
+                    j++;
+                if (i == j)
+                    lista.Add(i);
+                i++;  
+            }
+            WriteLine($"Tempo passado: {stopwatch.Elapsed}");
+            stopwatch.Stop();
+            Console.ReadKey();
+        }
+    }
+}
