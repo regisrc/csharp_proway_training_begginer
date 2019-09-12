6a86ce7eb155d2f2b2c08ad7338133468d6df24e
Sss41 <silvana.souza63@gmail.com>
2019-09-12 09:24:59 -0300

    Commit3

---
+++ LacosDeRepeticao/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var count = 0;
+            while (count <1000 )
+            {
+                Console.WriteLine($"numero:{count}");
+                count++; 
+                //enterno
+
+            }
+            Console.WriteLine(  " Terminou o loop");
+            Console.ReadKey();
+        }
+
+
+    }
+}
---
+++ UtilizandoFor/UtilizandoFor/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilizandoFor
+{
+    class Program
+    {
+        static void Main(string[] args)		
+        {
+            var nome = " Silvana";
+            var countFind = 0; 
+
+            for (int i = 1; i < 7; i++)
+            {
+                Console.WriteLine($"{nome[i-1]}-{nome[i]}-{nome[i+1]}");
+                if ((nome[i-1].ToString() +nome[i].ToString()) == "un")countFind++;
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
